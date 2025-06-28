e41763ab2b0b8759931d5b84fd4af9fd3cf83528
Faruque5698 <Ashaduzzaman5698@gmail.com>
2025-06-28 23:12:34 +0600

    Migration

---
+++ database/migrations/2025_06_28_162842_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug',10)->nullable()->index();
+            $table->string('name')->nullable()->index();
+            $table->string('image')->nullable();
+            $table->double('purchase_price')->default(0.000);
+            $table->double('sell_price')->default(0.000);
+            $table->enum('status', ['active', 'inactive'])->default('active')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
