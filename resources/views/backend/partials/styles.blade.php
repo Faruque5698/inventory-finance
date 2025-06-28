ef8c2124912f344997b8b16625a798271f0d453d
Faruque5698 <Ashaduzzaman5698@gmail.com>
2025-06-28 22:23:18 +0600

    Template mastering for backend

---
+++ resources/views/backend/partials/styles.blade.php
@@ -0,0 +1,3 @@
+<link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+<link href="{{asset('assets/backend')}}/css/styles.css" rel="stylesheet" />
+@stack('styles')
